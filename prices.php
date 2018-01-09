0775ab53f767a5acc806ea9f6a55bced9aa5ab11
Unknown <devarshi.j.raval@gmail.com>
2018-01-10 05:00:37 +0530

    Fixed CORS support

--- prices.php
+++ prices.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
