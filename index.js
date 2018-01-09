175e3ac0d04f4f46b99a81bb57be16529e742fde
Unknown <devarshi.j.raval@gmail.com>
2018-01-10 04:02:24 +0530

    Swith to php

--- index.js
+++
@@ -1,6 +0,0 @@
-var httpProxy = require('http-proxy');
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-httpProxy.createProxyServer({target:'http://api.farmrise.com/'}).listen(port);
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://api.farmrise.com/v1/users/fb3c7b8f-0305-4f9a-91cd-7dd0deeaca89/commodities/prices",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "accesstoken: 17018fd8-fd74-40f3-84c9-53e0d85527e0",
+    "appversion: 1.3.0",
+    "authorization: Basic ZGV2ZWxvcGVyOm1vYmlsZWFwcEBGUg==",
+    "cache-control: no-cache",
+    "deviceid: 9999999999999999",
+    "phonenumber: 8469696281",
+    "postman-token: b74d553b-577c-c8a7-3591-8b1649b57f8a",
+    "userid: fb3c7b8f-0305-4f9a-91cd-7dd0deeaca89"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
