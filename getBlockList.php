353dc05137fac8323c008257b5ad21d0ebe7e1bf
Unknown <devarshi.j.raval@gmail.com>
2018-04-08 16:08:14 +0530

    Farmer gov dom fix

--- getBlockList.php
+++ getBlockList.php
@@ -11,13 +11,19 @@ $dist = $_GET["DCode"];
 
 $curl = curl_init();
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "http://www.farmer.gov.in/FertilizerPesticideDealers.aspx?dealertype=P&SCode=".$state."&DCode=".$dist,
+  CURLOPT_URL => "http://www.farmer.gov.in/FertilizerPesticideDealers.aspx?dealertype=F&DealerStatus=E&SCode=".$state."&DCode=".$dist,
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "content-type: application/x-www-form-urlencoded",
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+    "Referer: http://www.farmer.gov.in/FertilizerPesticideDealersTotal.aspx?dealertype=F"
+  ),
   CURLOPT_COOKIEFILE =>'./cookie.txt',
   CURLOPT_COOKIEJAR => './cookie.txt',
 ));
